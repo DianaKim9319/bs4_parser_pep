941a4c420605426f92e608992b5289bd6d83b893
Diana Kim <Dianakim.1993@yandex.ru>
2024-01-17 20:06:37 +0900

    pep parser v1

--- src/main.py
+++ src/main.py
@@ -20,9 +20,12 @@ def whats_new(session):
     whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
     soup = get_soup(session, whats_new_url)
 
-    main_div = find_tag(soup, 'section', attrs={'id': 'what-s-new-in-python'})
-    div_with_ul = find_tag(main_div, 'div', attrs={'class': 'toctree-wrapper'})
-    sections_by_python = div_with_ul.find_all('li', attrs={'class': 'toctree-l1'})
+    main_div = find_tag(soup, 'section',
+                        attrs={'id': 'what-s-new-in-python'})
+    div_with_ul = find_tag(main_div, 'div',
+                           attrs={'class': 'toctree-wrapper'})
+    sections_by_python = div_with_ul.find_all('li',
+                                              attrs={'class': 'toctree-l1'})
 
     results = [('Ссылка на статью', 'Заголовок', 'Редактор, Автор')]
 
@@ -50,7 +53,8 @@ def latest_versions(session):
     """
     soup = get_soup(session, MAIN_DOC_URL)
 
-    sidebar = find_tag(soup, 'div', attrs={'class': 'sphinxsidebarwrapper'})
+    sidebar = find_tag(soup, 'div',
+                       attrs={'class': 'sphinxsidebarwrapper'})
     ul_tags = sidebar.find_all('ul')
 
     for ul in ul_tags:
@@ -85,8 +89,10 @@ def download(session):
     downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
     soup = get_soup(session, downloads_url)
 
-    table_tag = find_tag(soup, 'table', attrs={'class': 'docutils'})
-    pdf_a4_tag = find_tag(table_tag, 'a', attrs={'href': re.compile(r'.+pdf-a4\.zip$')})
+    table_tag = find_tag(soup, 'table',
+                         attrs={'class': 'docutils'})
+    pdf_a4_tag = find_tag(table_tag, 'a',
+                          attrs={'href': re.compile(r'.+pdf-a4\.zip$')})
 
     pdf_a4_link = pdf_a4_tag['href']
     archive_url = urljoin(downloads_url, pdf_a4_link)
@@ -112,8 +118,10 @@ def pep(session):
     """
     soup = get_soup(session, MAIN_PEP_URL)
 
-    main_section = find_tag(soup, 'section', attrs={'id': 'numerical-index'})
-    table = find_tag(main_section, 'table', attrs={'class': 'pep-zero-table docutils align-default'})
+    main_section = find_tag(soup, 'section',
+                            attrs={'id': 'numerical-index'})
+    table = find_tag(main_section, 'table',
+                     attrs={'class': 'pep-zero-table docutils align-default'})
     tbody_tag = find_tag(table, 'tbody')
     tr_tags = tbody_tag.find_all('tr')
 
@@ -130,7 +138,8 @@ def pep(session):
         soup = get_soup(session, pep_link)
 
         detail_tag = find_tag(soup, 'section', {'id': 'pep-content'})
-        dl_tag = find_tag(detail_tag, 'dl', {'class': 'rfc2822 field-list simple'})
+        dl_tag = find_tag(detail_tag, 'dl',
+                          {'class': 'rfc2822 field-list simple'})
 
         for tag in dl_tag:
             if tag.name == 'dt' and tag.text == 'Status:':
